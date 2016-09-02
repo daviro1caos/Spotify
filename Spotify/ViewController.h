8171935b5f8210f846f755610ccbf1b7bdcd0d5a
Daniel <Daniel@daniels-mac.local>
2016-09-02 16:21:18 -0600

    Initial Commit

---
+++ Spotify/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Spotify
+//
+//  Created by Daniel on 9/2/16.
+//  Copyright © 2016 DanielCompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Spotify/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Spotify
+//
+//  Created by Daniel on 9/2/16.
+//  Copyright © 2016 DanielCompany. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
